a7ddf0aa1f660685ad35eca7ddf2246b6c433c31
Tony <tshen815@gmail.com>
2022-04-08 16:36:19 -0500

    appease linter

--- src/js/experiments/gettingStartedGuide.js
+++ src/js/experiments/gettingStartedGuide.js
@@ -39,7 +39,7 @@ window.OptimizelyClient.getVariationName({
     if (
       window.location.pathname === '/docs/2.0/getting-started/' ||
       window.location.pathname ===
-      '/DD-456/finalize-getting-started-experiment-preview/2.0/getting-started/'
+        '/DD-456/finalize-getting-started-experiment-preview/2.0/getting-started/'
     ) {
       const articleContainer = $('.quickstart-guide');
       articleContainer.addClass('getting-started-full-width');
